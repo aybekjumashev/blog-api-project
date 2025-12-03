c50bfd5a91ddc33bdf1f81aa47b0fc349d9c093d
Aybek <aybek.djumashev@gmail.com>
2025-12-03 16:03:09 +0500

    .

--- config/settings.py
+++ config/settings.py
@@ -112,9 +112,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         # HOST atı "localhost" emes, al servis atı boladı!
         'HOST': os.environ.get('DB_HOST'),
         'PORT': '5432',
