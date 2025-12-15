20dc5f2811aefcd2cd28facffbe40d5744d95676
Aybek <aybek.djumashev@gmail.com>
2025-12-15 15:45:32 +0500

    .

--- posts/models.py
+++ posts/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
